b16adc51e416604678491febbaca3e2156520155
novakvova <novakvova@gmail>
2023-03-11 18:51:57 +0200

    Workin app product and category

--- web_app/shop/src/main/java/shop/entities/CategoryEntity.java
+++ web_app/shop/src/main/java/shop/entities/CategoryEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -20,5 +22,6 @@ public class CategoryEntity {
     private String image;
     @Column(length = 4000)
     private String description;
-
+    @OneToMany(mappedBy = "category")
+    private List<ProductEntity> products;
 }
